a8d5e77649b23b93ab561db779646d37090d27c6
gennadiySinigur <gennadiysinigur@gmail.com>
2022-01-16 21:59:14 +0200

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // purge: [
+  //   // Use *.tsx if using TypeScript
+  //   './pages/**/*.ts',
+  //   './components/**/*.tsx'
+  // ],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,html}",
+    "./components/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
