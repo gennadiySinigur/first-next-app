b80c069ea4b24c781c3b9e855450225eda249813
gennadiySinigur <gennadiysinigur@gmail.com>
2022-01-16 23:56:11 +0200

    added getStaticProps func, added blogs titles to the page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import { getSortedPostsData } from '../lib/posts';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-const Home: NextPage = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  };
+};
+
+const Home = ({allPostsData }: {
+  allPostsData: {
+    date: string,
+    title: string,
+    id: string
+  }[]
+} ) => {
   return (
       <Layout home>
         <Head>
           <title>{siteTitle}</title>
         </Head>
+
         <section className={utilStyles.headingMd}>
           <p>
             Hello, I'm Gennadiy. I'm a Frontend Developer. Now I'm working with
@@ -19,6 +36,21 @@ const Home: NextPage = () => {
             (This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
           </p>
+        </section>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+                <li className={utilStyles.listItem} key={id}>
+                  {title}
+                  <br />
+                  {id}
+                  <br />
+                  {date}
+                </li>
+            ))}
+          </ul>
         </section>
       </Layout>
   )
