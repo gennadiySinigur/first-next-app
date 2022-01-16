b5cbd6a2fe35d2261a05296dd7117bf8ab009a76
gennadiySinigur <gennadiysinigur@gmail.com>
2022-01-16 19:48:27 +0200

    created first-post.tsx

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import Head  from 'next/head';
+import Script from 'next/script';
+
+import Layout from "../../components/layout";
+
+const FirstPost = () => {
+  return (
+      <Layout>
+        <div>
+          <Head>
+            <title>First Post</title>
+          </Head>
+          <Script
+              src="https://connect.facebook.net/en_US/sdk.js"
+              strategy="lazyOnload"
+              onLoad={() =>
+                  console.log(`script loaded correctly, window.FB has been populated`)
+              }
+          />
+
+          <h1>First Post</h1>
+          <h2>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </h2>
+        </div>
+      </Layout>
+  );
+};
+
+export default FirstPost;
